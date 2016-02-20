ebce33e14b24ea596f844899544fb4fd7618299f
yulinliu <e9320.tw@gmail.com>
2016-02-20 14:10:12 +0800

    Create zh-TW.js

---
+++ lang/zh-TW.js
@@ -0,0 +1,14 @@
+var lang_zh = {
+  "yes" : "確定"
+	, "cancel" : "取消"
+	, "close" : "關閉"
+	, "delete" : "刪除"
+	, "remove" : "移除"
+	, "save" : "儲存"
+	, "start" : "開始"
+	, "pause" : "暫停"
+	, "wait" : "等待"
+	, "error" : "錯誤"
+	, "download" : "下載"
+};
+i18n.push("zh-TW", lang_zh);
