e902ab77ebe2461b763504816d6fbeb2f908f0f3
yulinliu <e9320.tw@gmail.com>
2016-02-20 14:09:02 +0800

    Create en.js

---
+++ lang/en.js
@@ -0,0 +1,14 @@
+var lang_en = {
+  "yes" : "Yes"
+	, "cancel" : "Cancel"
+	, "close" : "Close"
+	, "delete" : "Delete"
+	, "remove" : "Remove"
+	, "save" : "Save"
+	, "start" : "Start"
+	, "pause" : "Pause"
+	, "wait" : "Wait"
+	, "error" : "Error"
+	, "download" : "Download"
+};
+i18n.push("en", lang_en);
